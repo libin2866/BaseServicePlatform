e37e2a13c7be884c5d9eb66d0f6e729eb82668ae
libinPC <libin2866@hotmail.com>
2015-04-19 20:00:42 +0800

    git adjust

--- js/PedestrianAj.js
+++ js/PedestrianAj.js
@@ -1,4 +1,40 @@
+/**
+ * Created by libin on 15/4/19.
+ */
+function createXHR(){
+    if (typeof XMLHttpRequest != "undefined"){
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined"){
+        if (typeof arguments.callee.activeXString != "string"){
+            var versions = [ "MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
+                    "MSXML2.XMLHttp"],
+                i, len;
+            for (i=0,len=versions.length; i < len; i++){
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex){                    //跳过
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error("No XHR object available.");
+    } }
 
-function CreateHR(){
+function getConnectionAj() {
+    var xhr = createXHR();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
 
+                alert(xhr.responseText);
+            } else {
+                console.log("Request was unsuccessful: " + xhr.status);
+            }
+        }
+    };
+    xhr.open("get", "example.txt", true);
+    xhr.send(null);
 }
